cada1ca1f22a46191bcd8645d8b308f20046eab2
Poss111 <rixxroid@gmail.com>
2023-05-06 10:35:59 -0500

    Fixing test cases

--- test/models/lol_champions_data_test.dart
+++ test/models/lol_champions_data_test.dart
@@ -9,8 +9,8 @@ void main() {
   test('Test parsing of LoLChampionsData payload', () async {
     var input = await File("${Directory.current.path}/test/example-payloads/riot/lol-champions-payload.json").readAsString();
     var loLChampionsData = LoLChampionsData.fromJson(jsonDecode(input));
-    expect(loLChampionsData.data?.length, 163);
-    expect(loLChampionsData.data?['Aatrox']?.imageUrl, "http://ddragon.leagueoflegends.com/cdn/13.8.1/img/champion/Aatrox.png");
+    expect(loLChampionsData.data?.length, 5);
+    expect(loLChampionsData.data?['Aatrox']?.imageUrl, "http://ddragon.leagueoflegends.com/cdn/13.7.1/img/champion/Aatrox.png");
   });
 
 }
