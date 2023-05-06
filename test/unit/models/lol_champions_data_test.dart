9f034e4c732cc1ba0c3fcb615e9bcda1c5e3fac0
Poss111 <rixxroid@gmail.com>
2023-05-06 10:58:20 -0500

    Setting up build to run in parrallel

--- test/unit/models/lol_champions_data_test.dart
+++ test/unit/models/lol_champions_data_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Test parsing of LoLChampionsData payload', () async {
-    var input = await File("${Directory.current.path}/test/example-payloads/riot/lol-champions-payload.json").readAsString();
+    var input = await File("${Directory.current.path}/test/unit/example-payloads/riot/lol-champions-payload.json").readAsString();
     var loLChampionsData = LoLChampionsData.fromJson(jsonDecode(input));
     expect(loLChampionsData.data?.length, 5);
     expect(loLChampionsData.data?['Aatrox']?.imageUrl, "http://ddragon.leagueoflegends.com/cdn/13.7.1/img/champion/Aatrox.png");
