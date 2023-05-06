4aec09cb129eaeb21bd3838c2ada786e572a0950
Poss111 <rixxroid@gmail.com>
2023-05-06 12:07:04 -0500

    Adding threshold

---
+++ test/widget/threshold_golden_comparator.dart
@@ -0,0 +1,41 @@
+import 'dart:typed_data';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class ThresholdGoldenComparator extends LocalFileComparator {
+      /// Threshold above which tests will be marked as failing.
+      /// Ranges from 0 to 1, both inclusive.
+      final double threshold;
+    
+      ThresholdGoldenComparator(Uri testFile, this.threshold)
+          : assert(threshold >= 0 && threshold <= 1),
+            super(testFile);
+    
+      /// Copy of [LocalFileComparator]'s [compare] method, except for the fact that
+      /// it checks if the [ComparisonResult.diffPercent] is not greater than
+      /// [threshold] to decide whether this test is successful or a failure.
+      @override
+      Future<bool> compare(Uint8List imageBytes, Uri golden) async {
+        final result = await GoldenFileComparator.compareLists(
+          imageBytes,
+          await getGoldenBytes(golden),
+        );
+    
+        if (!result.passed && result.diffPercent <= threshold) {
+          debugPrint(
+            'A difference of ${result.diffPercent * 100}% was found, but it is '
+            'acceptable since it is not greater than the threshold of '
+            '${threshold * 100}%',
+          );
+    
+          return true;
+        }
+    
+        if (!result.passed) {
+          final error = await generateFailureOutput(result, golden, basedir);
+          throw FlutterError(error);
+        }
+        return result.passed;
+      }
+    }
